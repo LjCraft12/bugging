869f55bf5733870fc22ee931c089786d239fe5f8
Perry Craft <ljcraft12@gmail.com>
2017-06-19 15:04:09 -0400

    first commit of bugged out app

---
+++ app/app.component.ts
@@ -0,0 +1,7 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'my-app',
+    template: '<h1>Bugged Out</h1>'
+})
+export class AppComponent { }
---
+++ app/app.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { BugModule } from './bugs/bug.module';
+
+import { AppComponent } from './app.component';
+
+@NgModule({
+    imports: [
+        BrowserModule,
+        BugModule
+    ],
+    declarations: [ AppComponent ],
+    bootstrap: [ AppComponent ]
+})
+
+export class AppModule { }
